542d1484f7aea598705c1f82d5bbe3304ea95d1c
Swathi <swathisas005@gmail.com>
2026-06-10 20:43:30 +0530

    Name

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = () => {
     <div>
         <h1 className='text-center text-3xl'>User Profile</h1>
         <div className='max-w-md mx-auto mt-6 bg-white p-4 rounded shadow'>
-            <h2 className='text-xl font-bold mb-2'>John Doe</h2>
+            <h2 className='text-xl font-bold mb-2'>Swathi</h2>
             <p className='text-gray-600 mb-4'>Email: swathisas.com</p>
             <p className='text-gray-600 mb-4'>Phone: 123-456-7890</p>
             <div className='flex flex-col gap-4  '>
