8d246db2d64b11ebe9e4db807b2ac7ce78cdd06b
Swathi <swathisas005@gmail.com>
2026-06-17 21:00:20 +0530

    edited bg

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <>  <div>
-        <img src="/assets/bg.png" alt="Campus Lost and Found" className='w-full h-full object-cover' />
+        <img src="/assets/bg-small.png" alt="Campus Lost and Found" className='w-full h-full object-cover' />
        </div>
         <div className='text-center m-5 bg-amber-100 p-5 rounded-lg shadow-lg'>
             <h6 className='text-lg'>Welcome to</h6>
