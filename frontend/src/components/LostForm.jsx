5144152ee7c98f6b8a4b72947c296e0005ceb1e5
Pavan <pavansajeev746@gmail.com>
2026-06-17 23:36:02 +0530

    updated lostform

--- frontend/src/components/LostForm.jsx
+++ frontend/src/components/LostForm.jsx
@@ -1,18 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const LostForm = () => {
+
+  const {register,handleSubmit,reset,setValue}=useForm()
+  const navigate=useNavigate()
+
+  const onsubmit=async(data)=>{
+    try{
+    console.log(data)
+    const url="http://localhost:3000/lost"
+    const method='POST';
+    const response=await fetch(url,{
+      method:method,
+      headers:{
+        "Content-Type":"application/json",
+      },
+      body:JSON.stringify(data),
+    })
+    if(!response.ok)
+      throw new Error("Failed to add product")
+
+    alert("Product Added");
+    reset();
+    navigate('/lost');
+    }
+    catch(error){
+      console.log(error)
+      alert("Failed to add product");
+    }
+  };
+
   return (
     <div className="max-w-md mx-auto p-8 m-8 bg-white rounded shadow-2xl">
-      <h1 className='text-center text-3xl'>Enter details of lost item</h1>
-      <form className="flex flex-col gap-4 mt-4">
-       <label htmlFor="item-name">Item Name</label>
-       <input id="item-name" type="text" placeholder='Item Name'  className="border border-gray-400 p-2 rounded" />
-       <label htmlFor="location-found">Location Lost</label>
-       <input id="location-found" type="text" placeholder='Location Found'  className="border border-gray-400 p-2 rounded" />
-       <label htmlFor="description">Description</label>
-       <textarea id="description"  placeholder='Description'  className="border border-gray-400 p-2 rounded"/>
-       <label htmlFor="contact-info">Contact Information</label>
-       <input id="contact-info" type="text" placeholder='Contact Information'  className="border border-gray-400 p-2 rounded"/>
+      <form className="flex flex-col gap-4 mt-4" onSubmit={handleSubmit(onsubmit)}>
+        <h1 className='text-center text-3xl'>Enter details of lost item</h1>
+       <input 
+        {...register("name")}
+        placeholder='Product Name'
+        className='w-full rounded-2xl border-green-500 p-4'  
+      />
+      <textarea 
+        {...register("description")}
+        placeholder='Description'
+        className='w-full rounded-2xl border-green-500 p-4'
+      />
+      <input 
+        {...register("location")}
+        placeholder='Location'
+        className='w-full rounded-2xl border-green-500 p-4'
+      />
+      <input 
+        {...register("date")}
+        type='Date'
+        placeholder='Date'
+        className='w-full rounded-2xl border-green-500 p-4'
+      />
+      <input 
+        {...register("imageurl")}
+        placeholder='Image'
+        className='w-full rounded-2xl border-green-500 p-4'
+      />
        <button type='submit' className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
          Submit
        </button>
